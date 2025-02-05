b701887d63ffe26ac052b34c6c4cd3dda702ee47
Abhishek Dadoo <adadoo@gmail.com>
2025-02-05 22:44:47 +0800

    gtag

---
+++ gtag.js
@@ -0,0 +1,11 @@
+(function() {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-1SQ08XEGFW';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'G-1SQ08XEGFW');
+})();
