1b571afb86d7ff211c7c4ffd2ba2cf0c3a714c07
Abhishek Dadoo <adadoo@gmail.com>
2025-02-10 09:32:15 +0800

    gtm tag

---
+++ js/gtm-body.js
@@ -0,0 +1,17 @@
+// gtm-body.js
+export const initGTMBody = (containerId) => {
+    // Create the noscript iframe
+    const noscript = document.createElement('noscript');
+    const iframe = document.createElement('iframe');
+
+    iframe.src = 'https://www.googletagmanager.com/ns.html?id=${containerId}';
+    iframe.height = '0';
+    iframe.width = '0';
+    iframe.style.display = 'none';
+    iframe.style.visibility = 'hidden';
+
+    noscript.appendChild(iframe);
+
+    // Add it to the beginning of the body
+    document.body.insertBefore(noscript, document.body.firstChild);
+};
